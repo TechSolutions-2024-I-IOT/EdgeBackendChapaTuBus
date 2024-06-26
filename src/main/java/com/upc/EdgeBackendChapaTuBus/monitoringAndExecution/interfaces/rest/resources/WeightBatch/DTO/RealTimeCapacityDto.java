db22e2d25138a89e3de583d0bd4382471178507a
Sofia Alfaro <alfaros.sofia@gmail.com>
2024-06-25 22:52:13 -0500

    add: weight batch controller

---
+++ src/main/java/com/upc/EdgeBackendChapaTuBus/monitoringAndExecution/interfaces/rest/resources/WeightBatch/DTO/RealTimeCapacityDto.java
@@ -0,0 +1,8 @@
+package com.upc.EdgeBackendChapaTuBus.monitoringAndExecution.interfaces.rest.resources.WeightBatch.DTO;
+
+public record RealTimeCapacityDto(
+        Long id,
+        String capacity
+) {
+
+}
---
+++ src/main/java/com/upc/EdgeBackendChapaTuBus/monitoringAndExecution/interfaces/rest/transform/WeightBatch/WeightBatchCapacitiesResourceFromEntityAssembler.java
@@ -0,0 +1,22 @@
+package com.upc.EdgeBackendChapaTuBus.monitoringAndExecution.interfaces.rest.transform.WeightBatch;
+
+import com.upc.EdgeBackendChapaTuBus.monitoringAndExecution.domain.model.aggregates.WeightBatch;
+import com.upc.EdgeBackendChapaTuBus.monitoringAndExecution.interfaces.rest.resources.WeightBatch.DTO.RealTimeCapacityDto;
+import com.upc.EdgeBackendChapaTuBus.monitoringAndExecution.interfaces.rest.resources.WeightBatch.WeightBatchCapacitiesResource;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WeightBatchCapacitiesResourceFromEntityAssembler {
+    public static WeightBatchCapacitiesResource toResourceFromEntityAssembler(WeightBatch weightBatch){
+        List<RealTimeCapacityDto> capacityDtos = weightBatch.getRealTimeCapacities().stream()
+                .map(realTimeCapacity -> new RealTimeCapacityDto(realTimeCapacity.getId(), realTimeCapacity.getCapacity()))
+                .collect(Collectors.toList());
+
+        return new WeightBatchCapacitiesResource(
+                weightBatch.getId(),
+                weightBatch.getUnitBusId(),
+                capacityDtos
+        );
+    }
+}
